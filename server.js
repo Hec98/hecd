821de6850cef3518cecc77505f1bf49f78df8075
hecd98 <hector982015@gmail.com>
2021-05-23 14:33:48 -0500

    bootstrap implement

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8000;
+
 app.use(express.static(`${__dirname}/dist/hecd`));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(`${__dirname}/dist/hecd/index.html`));
-});
-app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req,res) => res.sendFile(path.join(`${__dirname}/dist/hecd/index.html`)));
+
+const hora = new Date().getHours();
+const minutos = new Date().getMinutes();
+const segundos = new Date().getSeconds();
+
+app.listen(port, () => console.log(`Escuchando peticiones en el puerto ${port} ${hora}:${minutos}:${segundos}`));
